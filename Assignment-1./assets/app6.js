eae9d03dcde558bab0cf67f5f47b9bb36d17a32d
Maham Tahir <106860878+MahamTahir31@users.noreply.github.com>
2023-05-06 23:17:56 +0500

    Create app6.js

---
+++ Assignment-1./assets/app6.js
@@ -0,0 +1,5 @@
+// Declare a variable to store the email address
+var Email = "mahamst31@gmail.com";
+
+// Display a message in an alert box using string concatenation
+alert("My email address is: " + Email);
