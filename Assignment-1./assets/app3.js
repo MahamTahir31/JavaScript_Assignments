7026fec00427bbf60a178022f07c14579deaf467
Maham Tahir <106860878+MahamTahir31@users.noreply.github.com>
2023-05-06 23:16:26 +0500

    Create app3.js

---
+++ Assignment-1./assets/app3.js
@@ -0,0 +1,8 @@
+// Declare a variable called "message"
+var message;
+
+// Assign "Hello World" to the "message" variable
+message = "Hello World";
+
+// Display the message in the console
+console.log(message);
