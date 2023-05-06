79a76e3a3ed062c8a61917bfa1e327a8ca0b8616
Maham Tahir <106860878+MahamTahir31@users.noreply.github.com>
2023-05-06 23:18:19 +0500

    Create app7.js

---
+++ Assignment-1./assets/app7.js
@@ -0,0 +1,5 @@
+// Declare a variable to store the book title
+var book = "A smarter way to learn JavaScript";
+
+// Display a message in an alert box
+alert("This is very helpful book namely --> " + book);
