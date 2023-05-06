a34705e9ccd46c6bcb67558d793ba931d1879f8e
Maham Tahir <106860878+MahamTahir31@users.noreply.github.com>
2023-05-06 23:18:51 +0500

    Create app8.js

---
+++ Assignment-1./assets/app8.js
@@ -0,0 +1,8 @@
+// Create a new HTML element to hold the message
+var messageElement = document.createElement("p");
+
+// Set the HTML content of the element to a message
+messageElement.innerHTML = "Yah! I can write <strong>HTML</strong> content through JS";
+
+// Append the element to the body of the document
+document.body.appendChild(messageElement);
