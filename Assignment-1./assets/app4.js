a0a2670142207bbfea06cebafd2be7dc0c3af488
Maham Tahir <106860878+MahamTahir31@users.noreply.github.com>
2023-05-06 23:17:02 +0500

    Create app4.js

---
+++ Assignment-1./assets/app4.js
@@ -0,0 +1,10 @@
+// Save student's bio data in variables
+var Name = "Maham";
+var Age = 21;
+var Gender = "Female"
+
+
+// Display the data in alert boxes
+alert("Name: " + Name);
+alert("Age: " + Age);
+alert("Gender: " + Gender);
