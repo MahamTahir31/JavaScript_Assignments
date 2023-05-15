7303ab73fba917da13ad1ff18e4d79adcd153010
Maham Tahir <106860878+MahamTahir31@users.noreply.github.com>
2023-05-15 19:55:28 +0500

    Add files via upload

---
+++ Assignment-4/assets/app2.js
@@ -0,0 +1,2 @@
+let a, name1, _quantity, $amount, prod_name;
+let 12a, 1name, % res, amount - paid, not#incl;
---
+++ Assignment-4/assets/app3.js
@@ -0,0 +1,5 @@
+document.write("Rules for naming JS variables");
+document.write("Variable name can only contain $,_,numbers after alphabet, upper case and lower case alphabets - For example $my_1stVariable");
+document.write("Variable must begin with _, $, any alphabet - For example $name, _name or name");
+document.write("variables names are case sensitive");
+document.write("variables names should not be JS keywords");
