0f6dbf3f4816722c96e28850a522f4dc1d959f1c
Maham Tahir <106860878+MahamTahir31@users.noreply.github.com>
2023-05-15 18:46:36 +0500

    Create app4.js

---
+++ Assignment-3/assets/app4.js
@@ -0,0 +1,5 @@
+let Visitors_name, Title, Quantity;
+Visitors_name = 'Maham';
+Title = 'T-shirt';
+Quantity = 1;
+document.write(Visitors_name + " ordered " + Quantity + " " + Title + " on XYZ Clothing Store ");
